90f39e6903b7c88625041e8d708b742f46f8cec8
ankit.charls <ankitcharls04@gmail.com>
2024-07-19 15:09:57 -0600

    ceilingfan- adding support for swagger.

--- src/main/java/com/teacup/ceilingfan/controller/FanController.java
+++ src/main/java/com/teacup/ceilingfan/controller/FanController.java
@@ -1,6 +1,7 @@
 package com.teacup.ceilingfan.controller;
 
 import com.teacup.ceilingfan.service.FanService;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/fan")
+@Tag(name = "Ceiling Fan Controller", description = "API to control a ceiling fan")
 public class FanController {
 
     @Autowired
@@ -29,5 +31,15 @@ public class FanController {
     public String pullDirectionCord() {
         fanService.pullDirectionCord();
         return fanService.getStatus();
+    }
+
+    @GetMapping("/speed")
+    public int getSpeed() {
+        return fanService.getSpeed();
+    }
+
+    @GetMapping("/direction")
+    public boolean isClockwise() {
+        return fanService.isClockwise();
     }
 }
